5c8ae0ddd58795c3d896613401b5d56cded107a5
Bluemoon1978 <40301962+Bluemoon1978@users.noreply.github.com>
2019-05-21 10:39:11 +0200

    Update setcab.sh

--- scripts/setcab.sh
+++ scripts/setcab.sh
@@ -1,7 +1,11 @@
-apt-get install libcap2-bin arp-scan bluetooth bluez libbluetooth-dev libudev-dev net-tools
+
+#!/bin/bash
+
 setcap cap_net_admin,cap_net_raw,cap_net_bind_service=+eip $(eval readlink -f `which arp-scan`)
 setcap cap_net_admin,cap_net_raw,cap_net_bind_service=+eip $(eval readlink -f `which node`)
 setcap cap_net_admin,cap_net_raw,cap_net_bind_service=+eip $(eval readlink -f `which arp`)
 setcap cap_net_admin,cap_net_raw,cap_net_bind_service=+eip $(eval readlink -f `which hcitool`)
 setcap cap_net_admin,cap_net_raw,cap_net_bind_service=+eip $(eval readlink -f `which hciconfig`)
 setcap cap_net_admin,cap_net_raw,cap_net_bind_service=+eip $(eval readlink -f `which l2ping`)
+
+exit 0
