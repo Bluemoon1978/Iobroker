a898f0c312d9cf77aafac6f477c9eada11937025
Bluemoon1978 <40301962+Bluemoon1978@users.noreply.github.com>
2019-08-16 17:57:02 +0200

    Update setup_packages.sh

--- scripts/setup_packages.sh
+++ scripts/setup_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y  $(cat /opt/scripts/.packages)
+apt update
+apt install -y  $(cat /opt/scripts/.packages)
 rm -rf /var/lib/apt/lists/*
 rm -f /opt/scripts/.packages
 
